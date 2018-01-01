ce7d8e6de17c0addcb4ea275cad28e243caa5e2c
mac <mac@macdeMacBook-Pro.local>
2018-01-01 22:13:16 +0800

    first step from Java to Kotlin .

---
+++ JumpJumpKotlin/src/com/company/jumpjump/Constants.kt
@@ -0,0 +1,11 @@
+package com.company.jumpjump
+
+/**
+ * adb所在位置
+ */
+val ADB_PATH = "/Users/mac/Library/Android/sdk/platform-tools/adb"
+
+/**
+ * 截屏文件所在位置
+ */
+val SCREENSHOT_LOCATION = "/Users/mac/screenshoot.png"
